533538abde0cfc2d55fad09e29ef42e0bb53d378
Roberto Díaz <r.r.1994a@gmail.com>
2017-06-21 11:59:01 +0100

    changed db name
    Former-commit-id: 5bbf947e1b3de348807f33d38a97b748d3177dbb

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import os
 if 'RDS_DB_NAME' in os.environ:
-            db = os.environ['RDS_DB_NAME']
+            db = os.environ['dcss-analyzer']
             user = os.environ['RDS_USERNAME']
             pwd = os.environ['RDS_PASSWORD']
             host = os.environ['RDS_HOSTNAME']
